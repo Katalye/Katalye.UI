5046118efb4edb62e7aa1ba244019231fc97e394
Mark Lopez <m@silvenga.com>
2019-01-20 13:16:10 -0600

    Updated date-time converters to pass through null values.

--- src/resources/converters/date-time.ts
+++ src/resources/converters/date-time.ts
@@ -2,18 +2,27 @@ import moment from "moment";
 
 export class DateFormatValueConverter {
     public toView(value: string, format: string = "l LT") {
+        if (!value) {
+            return null;
+        }
         return moment(value).format(format);
     }
 }
 
 export class TimeAgoValueConverter {
     public toView(value: string, withoutSuffix: boolean) {
+        if (!value) {
+            return null;
+        }
         return moment(value).fromNow(withoutSuffix);
     }
 }
 
 export class DurationValueConverter {
     public toView(value: number, format: string = "mm:ss.SSS") {
+        if (!value) {
+            return null;
+        }
         return moment.utc(value).format(format);
     }
 }
